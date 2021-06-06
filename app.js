1f974d0994370568ebb3c97b1013805ca9969423
balaji sai kumar <balajisaikumar3@gmail.com>
2021-06-06 16:03:02 +0530

    delete a tour

--- app.js
+++ app.js
@@ -76,6 +76,23 @@ app.patch('/api/v1/tours/:id', (req, res) => {
     },
   });
 });
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+
+  if (id > toursData.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: null,
+  });
+});
+
 const port = 3000;
 
 app.listen(port, () => {
