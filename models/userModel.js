fa77c0dbdb6090713c9b0a05bee37094599ca4df
balaji sai kumar <balajisaikumar3@gmail.com>
2021-06-12 21:46:38 +0530

    password encryption completed

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
+const bcrypt = require('bcryptjs');
+
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -27,9 +29,22 @@ const userSchema = new mongoose.Schema({
       validator: function (el) {
         return el === this.password; //password===password
       },
+      message: 'Passwords are not same',
     },
   },
 });
 
+//NOTE pre middleware
+userSchema.pre('save', async function (next) {
+  //Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  //Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  //delete passwordConfirm
+  this.passwordConfirm = undefined;
+  next();
+});
 const User = mongoose.model('User', userSchema);
 module.exports = User;
