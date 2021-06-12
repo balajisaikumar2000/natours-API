ae5ee6d5e96c9ecd402a65b53a3fb23052b22cee
balaji sai kumar <balajisaikumar3@gmail.com>
2021-06-12 20:18:01 +0530

    basic auth response

---
+++ models/userModel.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please tell us your email'],
+    unique: true,
+    lowecase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      //NOTE:- this only works on SAVE()
+      validator: function (el) {
+        return el === this.password; //password===password
+      },
+    },
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const dotenv = require('dotenv');
+
 process.on('uncaughtException', (err) => {
   console.log('UNCAUGHT EXCEPTION shutting down😣😣');
   console.log(err.name, err.message);
